dff9d11b7252bdef8b67522c1b1e1b6d8ef3336b
Kevin DeJong <kddejong@amazon.com>
2025-05-24 07:52:49 -0700

    Try my construct

---
+++ lib/constructs/index.ts
@@ -0,0 +1,1 @@
+export {MySecureBucket} from './bucket';
--- lib/grc442-cdk-stack.ts
+++ lib/grc442-cdk-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import { NagSuppressions } from 'cdk-nag';
+// import * as s3 from 'aws-cdk-lib/aws-s3';
+// import { NagSuppressions } from 'cdk-nag';
+import { MySecureBucket } from './constructs';
 
 export class Grc442CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'MyBucket', {});
+    new MySecureBucket(this, 'bucket', {})
 
-    NagSuppressions.addResourceSuppressions(bucket, [
-      { id: 'AwsSolutions-S1', reason: 'lorem ipsum' },
-    ]);
   }
 }
