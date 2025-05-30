a4bd4a31ab82aafc9e524dde05b48394c1e7e32f
Kevin DeJong <kddejong@amazon.com>
2025-05-30 11:16:08 -0700

    Block public access

--- lib/grc442-cdk-stack.ts
+++ lib/grc442-cdk-stack.ts
@@ -9,6 +9,7 @@ export class Grc442CdkStack extends cdk.Stack {
         new s3.Bucket(this, 'bucket', {
             encryption: s3.BucketEncryption.KMS_MANAGED,
             enforceSSL: true,
+            publicReadAccess: false,
         });
     }
 }
