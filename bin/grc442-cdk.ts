d8a6d630a0315d515618af5344ab2412f9eb0a07
Kevin DeJong <kddejong@amazon.com>
2025-06-04 15:18:09 -0700

    My starting spot

--- bin/grc442-cdk.ts
+++ bin/grc442-cdk.ts
@@ -1,23 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Grc442CdkStack } from '../lib/grc442-cdk-stack';
-import { MyCompanyChecker } from '../lib/aspects';
-import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
 
 const app = new cdk.App();
 
-cdk.Aspects.of(app).add(new MyCompanyChecker());
-
-cdk.Aspects.of(app).add(new AwsSolutionsChecks());
-
-const stack = new Grc442CdkStack(app, 'Grc442CdkStack', {});
-
-NagSuppressions.addStackSuppressions(
-    stack,
-    [
-        {
-            id: "AwsSolutions-S1",
-            reason: "For demo purposes",
-        }
-    ]
-)
+new Grc442CdkStack(app, 'Grc442CdkStack', {});
--- lib/grc442-cdk-stack.ts
+++ lib/grc442-cdk-stack.ts
@@ -6,10 +6,6 @@ export class Grc442CdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new s3.Bucket(this, 'bucket', {
-            encryption: s3.BucketEncryption.KMS_MANAGED,
-            enforceSSL: true,
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-        });
+        new s3.Bucket(this, 'bucket');
     }
 }
