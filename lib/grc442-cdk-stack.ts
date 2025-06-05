ef34e60de73895e729a1315d7ae72b62566d1805
Kevin DeJong <kddejong@amazon.com>
2025-06-04 19:29:46 -0700

    Initial commit

--- lib/grc442-cdk-stack.ts
+++ lib/grc442-cdk-stack.ts
@@ -1,12 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import { MyCompanyBucket } from './constructs';
 
 export class Grc442CdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new MyCompanyBucket(this, 'bucket', {});
+        new s3.Bucket(this, 'bucket', {});
     }
 }
