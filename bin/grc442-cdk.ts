d97d80825c55bbdb912c47b47fa1f17181e22c29
Kevin DeJong <kddejong@amazon.com>
2025-06-04 19:10:56 -0700

    Add AWS Solutions best practices

--- bin/grc442-cdk.ts
+++ bin/grc442-cdk.ts
@@ -2,9 +2,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Grc442CdkStack } from '../lib/grc442-cdk-stack';
 import { MyCompanyChecker } from '../lib/aspects';
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
 
 const app = new cdk.App();
 
 cdk.Aspects.of(app).add(new MyCompanyChecker());
 
-new Grc442CdkStack(app, 'Grc442CdkStack', {});
+cdk.Aspects.of(app).add(new AwsSolutionsChecks());
+
+const stack = new Grc442CdkStack(app, 'Grc442CdkStack', {});
+
+NagSuppressions.addStackSuppressions(stack, [
+    {
+        id: "AwsSolutions-S1",
+        reason: "For the demo",
+    }
+])
