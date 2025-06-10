249c1b2170e69e10cd7f1e2c6ebe273d255f53bb
Kevin DeJong <kddejong@amazon.com>
2025-06-10 13:17:44 -0700

    My secure concstruct

--- lib/grc442-cdk-stack.ts
+++ lib/grc442-cdk-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { MyCompanyBucket } from './constructs';
 
 export class Grc442CdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new s3.Bucket(this, 'bucket', {
-            encryption: s3.BucketEncryption.KMS_MANAGED,
-            enforceSSL: true,
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-            versioned: true,
-        });
+        new MyCompanyBucket(this, 'bucket');
     }
 }
