e0941c0323afc03c10dd46dc6f8d8cebd5292763
Kevin DeJong <kddejong@amazon.com>
2025-06-16 11:44:23 -0700

    Start

--- bin/grc442-cdk.ts
+++ bin/grc442-cdk.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Grc442CdkStack } from '../lib/grc442-cdk-stack';
+import { MyCompanyChecker } from '../lib/aspects';
 
 const app = new cdk.App();
+
+cdk.Aspects.of(app).add(new MyCompanyChecker());
 
 new Grc442CdkStack(app, 'Grc442CdkStack', {});
